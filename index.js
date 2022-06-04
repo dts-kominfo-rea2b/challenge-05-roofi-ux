3ee1c821da2fde07a87c0a7cf9fe645d09de0ec6
Firdha Roofi Irawan <roofiii96@gmail.com>
2022-06-04 15:49:23 +0700

    Update index.js

--- index.js
+++ index.js
@@ -6,15 +6,32 @@ const names = ["Halo", "Angel", "Nyoman", "Ketut", "Aisyah"];
 // "1. Nama"
 // "2. Nama"
 // ...
-const sorter = null;
+
+const sorter = (data, nama) => {
+    let urutan = nama(data);
+    let ambil = [];
+    for (i = 0; i < data.length; i++) {
+        ambil.push(`${i+1}. ${urutan[i]}`)
+        ambil[i].toString();
+    }
+    return ambil;
+};
 
 // TODO: Fungsi untuk mengurutkan array of string secara ascending
 // mengembalikan array yang sudah diurutkan
-const sortAscending = null;
+
+const sortAscending = (names) => {
+    return names.sort();
+};
 
 // TODO: Fungsi untuk mengurutkan array of string secara descending
 // mengembalikan array yang sudah diurutkan
-const sortDescending = null;
+
+const sortDescending = (names) => {
+    let sortDesc = names;
+    sortDesc.sort();
+    return sortDesc.reverse();
+};
 
 // ! JANGAN DIMODIFIKASI
 (function main() {
